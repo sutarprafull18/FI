7eae7653a965e571d379e87ae3c324c858e5b0dc
Prafull Sutar <sutarprafull18@gmail.com>
2025-03-18 13:09:13 +0530

    Update product_1.py

--- app/product_1.py
+++ app/product_1.py
@@ -1,18 +1,17 @@
 import streamlit as st
-from streamlit_image_zoom import st_image_zoom
 import os
 
 def show_product_detail():
-    st.title("Product 2 Details")
-    st.write("Description of Product 2.")
+    st.title("Product 1 Details")
+    st.write("Description of Product 1.")
 
-    # Path to the images folder for Product 2
-    image_folder = "images/product_2"
+    # Path to the images folder for Product 1
+    image_folder = "images/product_1"
 
     # List all images in the folder
     image_files = sorted(os.listdir(image_folder))
 
-    # Display images with zoom functionality
+    # Display each image
     for image_file in image_files:
         image_path = os.path.join(image_folder, image_file)
-        st_image_zoom(image_path, caption=image_file)
+        st.image(image_path, caption=image_file, use_column_width=True)

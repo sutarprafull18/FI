bea3753618144cd7d2b0bb9fc0fdc8e89d915ac0
Prafull Sutar <sutarprafull18@gmail.com>
2025-03-18 13:27:27 +0530

    Update and rename product_1.py to Sahara.py

--- app/Sahara.py
+++ app/Sahara.py
@@ -2,11 +2,11 @@ import streamlit as st
 import os
 
 def show_product_detail():
-    st.title("Product 1 Details")
-    st.write("Description of Product 1.")
+    st.title("Sahara Details")
+    st.write("Description of Sahara.")
 
-    # Path to the images folder for Product 1
-    image_folder = "images/product_1"
+    # Path to the images folder for Sahara
+    image_folder = "images/Sahara"
 
     # List all images in the folder
     image_files = sorted(os.listdir(image_folder))

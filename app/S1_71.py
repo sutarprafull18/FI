a419aa3bc7ded79460bde17a98ab05606651e7e6
Prafull Sutar <sutarprafull18@gmail.com>
2025-03-18 13:27:57 +0530

    Update and rename product_2.py to S1_71.py

--- app/S1_71.py
+++ app/S1_71.py
@@ -2,11 +2,11 @@ import streamlit as st
 import os
 
 def show_product_detail():
-    st.title("Product 2 Details")
-    st.write("Description of Product 2.")
+    st.title("S1_71 Details")
+    st.write("Description of S1_71.")
 
-    # Path to the images folder for Product 2
-    image_folder = "images/product_2"
+    # Path to the images folder for S1_71
+    image_folder = "images/S1_71"
 
     # List all images in the folder
     image_files = sorted(os.listdir(image_folder))

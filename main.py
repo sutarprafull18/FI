34061cf10284bcd21e01b9598cfd5a38df73c344
Prafull Sutar <sutarprafull18@gmail.com>
2025-03-18 13:26:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import sys
 import os
+
+# Add the app directory to the system path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
 
 # Set page configuration
 st.set_page_config(
@@ -16,22 +20,22 @@ st.sidebar.markdown("---")
 if st.sidebar.button("🏠 Home"):
     st.session_state.page = 'home'
 
-if st.sidebar.button("🛋️ Product 1"):
-    st.session_state.page = 'product_1'
+if st.sidebar.button("🛋️ S1_71"):
+    st.session_state.page = 'S1_71'
 
-if st.sidebar.button("🛋️ Product 2"):
-    st.session_state.page = 'product_2'
+if st.sidebar.button("🛋️ Sahara"):
+    st.session_state.page = 'Sahara'
 
 # Display the selected page
 if st.session_state.get('page') == 'home':
     from app import home
     home.show_homepage()
-elif st.session_state.get('page') == 'product_1':
-    from app import product_1
-    product_1.show_product_detail()
-elif st.session_state.get('page') == 'product_2':
-    from app import product_2
-    product_2.show_product_detail()
+elif st.session_state.get('page') == 'S1_71':
+    from app import S1_71
+    S1_71.show_product_detail()
+elif st.session_state.get('page') == 'Sahara':
+    from app import Sahara
+    Sahara.show_product_detail()
 else:
     from app import home
     home.show_homepage()
